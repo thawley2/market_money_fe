cedefc95b7b2779cc690584acf92333bf6243bda
Thomas Hawley <thawley22@turing.edu>
2023-05-25 11:46:21 -0600

    feat: Add market method to retrieve one market

--- app/facades/market_money_facade.rb
+++ app/facades/market_money_facade.rb
@@ -1,10 +1,18 @@
 class MarketMoneyFacade
 
-    def markets
-      @_markets ||= markets_data.map do |market_data|
-        Market.new(market_data)
-      end
+  def initialize(id = nil)
+    @id = id
+  end
+
+  def markets
+    @_markets ||= markets_data.map do |market|
+      Market.new(market)
     end
+  end
+
+  def market
+    @_market ||= Market.new(market_data)
+  end
 
   private
     def service
@@ -13,5 +21,9 @@ class MarketMoneyFacade
 
     def markets_data
       @_markets_data ||= service.find_markets[:data]
+    end
+
+    def market_data
+      @_market_data ||= service.find_market(@id)[:data]
     end
 end
