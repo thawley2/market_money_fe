67efae39d56e71a057a71fa6dc8f490d4b9c494c
Thomas Hawley <thawley22@turing.edu>
2023-05-25 14:11:44 -0600

    feat: Create Vendor poro

---
+++ app/poros/vendor.rb
@@ -0,0 +1,12 @@
+class Vendor
+  attr_reader :name, :description, :contact_name, :contact_phone, :credit_accepted, :id
+
+  def initialize(data)
+    @id = data[:id]
+    @name = data[:attributes][:name]
+    @description = data[:attributes][:description]
+    @contact_name = data[:attributes][:contact_name]
+    @contact_phone = data[:attributes][:contact_phone]
+    @credit_accepted = data[:attributes][:credit_accepted]
+  end
+end
