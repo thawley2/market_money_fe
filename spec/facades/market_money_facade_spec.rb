568f01b1e005fcba014a934dab08ef1ea529c880
Thomas Hawley <thawley22@turing.edu>
2023-05-25 11:39:32 -0600

    test: Add test for market

--- spec/facades/market_money_facade_spec.rb
+++ spec/facades/market_money_facade_spec.rb
@@ -2,14 +2,28 @@ require 'rails_helper'
 
 RSpec.describe MarketMoneyFacade do
   describe 'Markets' do
-    it 'returns an array of market objects for all markets' do
+    it 'returns an array of market objects for all markets', :vcr do
       markets = MarketMoneyFacade.new.markets
-      
+
       expect(markets).to be_an Array
       expect(markets.first).to be_a Market
       
       market = markets.first
 
+      expect(market.name).to be_a String
+      expect(market.street).to be_a String
+      expect(market.city).to be_a String
+      expect(market.county).to be_a String
+      expect(market.state).to be_a String
+      expect(market.zip).to be_a String
+    end
+  end
+
+  describe 'Market' do
+    it 'returns a single market object', :vcr do
+      market = MarketMoneyFacade.new(322474).market
+
+      expect(market).to be_a Market
       expect(market.name).to be_a String
       expect(market.street).to be_a String
       expect(market.city).to be_a String
