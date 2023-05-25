1a276abf216df8dd9abbdd095d3b2b10f6c46497
Thomas Hawley <thawley22@turing.edu>
2023-05-25 10:38:21 -0600

    Add VCR and webmock

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -66,6 +66,5 @@ end
 VCR.configure do |config|
   config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
   config.hook_into :webmock
-  config.filter_sensitive_data('<TMBD_API_KEY>') { ENV['TMBD_API_KEY'] }
   config.configure_rspec_metadata!
 end
